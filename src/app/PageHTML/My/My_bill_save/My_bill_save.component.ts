dd0a00063b18bcafe8def0700eeff93baca7be4a
Nains <jason20226@gmail.com>
2020-11-16 18:08:07 +0800

    up

--- src/app/PageHTML/My/My_bill_save/My_bill_save.component.ts
+++ src/app/PageHTML/My/My_bill_save/My_bill_save.component.ts
@@ -1,3 +1,4 @@
+import { element } from 'protractor';
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
@@ -7,12 +8,25 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class My_bill_saveComponent implements OnInit {
   @Output() childEvent: EventEmitter<any> = new EventEmitter();
+  checkbx: any = [];
   constructor() { }
-
+  checksave(x, y) {
+    let obj = x.getElementsByTagName('li');
+    this.checkbx = [];
+    for (let i = 0; i < obj.length; i++) {
+      if (i == y) {
+        this.checkbx[i] = true;
+      } else {
+        this.checkbx[i] = false;
+      }
+    }
+  }
   change() {
     this.childEvent.emit(false);
   }
   ngOnInit() {
+    let ulName = document.getElementById('savebox');
+    this.checksave(ulName, 0);
   }
 
 }
