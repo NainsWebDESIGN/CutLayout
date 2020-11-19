746aac533b870ca32cbd3b5e85f93d00725d9935
Nains <jason20226@gmail.com>
2020-11-19 15:50:05 +0800

    up

--- src/app/PageHTML/Balls/basball/basball.component.ts
+++ src/app/PageHTML/Balls/basball/basball.component.ts
@@ -13,3 +13,45 @@ export class BasballComponent implements OnInit {
   }
 
 }
+
+@Component({
+  selector: 'basball-header',
+  templateUrl: './basball-header.html',
+  styleUrls: ['./basball.component.css']
+})
+export class BasballHeader implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+@Component({
+  selector: 'basball-content',
+  templateUrl: './basball-content.html',
+  styleUrls: ['./basball.component.css']
+})
+export class BasballContent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+@Component({
+  selector: 'basball-right',
+  templateUrl: './basball-right.html',
+  styleUrls: ['./basball.component.css']
+})
+export class BasballRight implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
