5230416ec1cd75c5da9c286807fc6cbed26371ad
Nains <jason20226@gmail.com>
2020-12-02 10:30:08 +0800

    up

--- src/app/Error/Error.component.ts
+++ src/app/Error/Error.component.ts
@@ -1,5 +1,6 @@
 import { Location } from '@angular/common';
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewEncapsulation } from '@angular/core';
+declare var anime: any;
 
 @Component({
   selector: 'app-Error',
@@ -7,11 +8,32 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./Error.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class ErrorComponent implements OnInit {
+export class ErrorComponent implements OnInit, AfterViewInit {
 
   constructor(private backpage: Location) { }
   back() {
     this.backpage.back();
+  }
+  ngAfterViewInit() {
+    anime({
+      targets: '.row svg',
+      translateY: 10,
+      autoplay: true,
+      loop: true,
+      easing: 'easeInOutSine',
+      direction: 'alternate'
+    });
+
+    anime({
+      targets: '#zero',
+      translateX: 10,
+      autoplay: true,
+      loop: true,
+      easing: 'easeInOutSine',
+      direction: 'alternate',
+      scale: [{ value: 1 }, { value: 1.4 }, { value: 1, delay: 250 }],
+      rotateY: { value: '+=180', delay: 200 },
+    });
   }
   ngOnInit() {
   }
